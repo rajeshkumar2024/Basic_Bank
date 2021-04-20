8bb83780ba1ce95bf82574ef1e079ddcfca7ac79
rajeshkumar2024 <80545227+rajeshkumar2024@users.noreply.github.com>
2021-04-21 00:18:27 +0530

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,3 +1,3 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=id16579511_misc','id16579511_rajrk2024', 'I7<$&<H>-R5v1bCP');
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=id16579511_misc','id16579511_rajrk2024', 'Hidden');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
